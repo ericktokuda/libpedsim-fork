a85eec8e9d271aecaeb6650a51512e8d474ab8cb
Grace <tokudaek@gmail.com>
2017-03-21 14:19:21 -0400

    Update gui to not follow agents

--- sample/src/config.cpp
+++ sample/src/config.cpp
@@ -9,8 +9,7 @@
 
 using namespace std;
 
-/// \date    2012-01-11
-Config::Config() { // make sure this reflects what is set as default in the user interface!  --chgloor 2012-01-13
+Config::Config() {
     guiShowWaypoints = false;
     simWallForce = 50;
     simPedForce = 30;
@@ -21,25 +20,21 @@ Config::Config() { // make sure this reflects what is set as default in the user
     showDirection = true;
     showTree = false;
     simh = 0.4;
-    followAgent = true;
+    followAgent = false;
 }
 
-/// \date    2012-01-11
 void Config::setGuiShowWaypoints(bool value) {
     guiShowWaypoints = value;
 }
 
-/// \date    2012-01-11
 void Config::setSimWallForce(double value) {
     simWallForce = value;
 }
 
-/// \date    2012-01-11
 void Config::setSimPedForce(double value) {
     simPedForce = value;
 }
 
-/// \date    2012-01-11
 void Config::setSimSpeed(int value) {
     simSpeed = value;
 }
